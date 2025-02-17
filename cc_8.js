8c19e4902d60cb82237ae40699166ef2c9651462
mrohling13 <malachi.rohling@yahoo.com>
2025-02-16 19:28:08 -0500

    Task 6 - Identifying Large Transactions

--- cc_8.js
+++ cc_8.js
@@ -91,4 +91,20 @@ console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Pay
 
 console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
 
+// Task 6: Higher-Order Functions
+
+function filterLargeTransactions(transactions, filterFunction) { // Function filters Large Transacations
+    return transactions.filter(filterFunction);
+}
+
+// Test Data for Task 6 
+
+let transactions = [200, 1500, 3200, 800, 2500];
+console.log(filterLargeTransactions(transactions, amount => amount > 1000)); // Expected output: [1500, 3200, 2500]
+
+
+
+
+
+
 
